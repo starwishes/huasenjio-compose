606f85a9e0fdd754dc40637e5360f00005962e9e
starwishes <31697768+starwishes@users.noreply.github.com>
2024-08-26 16:51:38 +0800

    Update WrapSidebar.vue

--- huasen-frontend/portal/src/components/content/wrap/sidebar/WrapSidebar.vue
+++ huasen-frontend/portal/src/components/content/wrap/sidebar/WrapSidebar.vue
@@ -14,19 +14,7 @@
       <li class="shadow">
         <i class="iconfont icon-a-unfoldcross-line" @click="handleNavbar"> </i>
       </li>
-      <li class="shadow">
-        <i class="iconfont icon-github" @click="goStorage"> </i>
-      </li>
-      <li class="shadow">
-        <i class="iconfont icon-weixin" @mouseenter="showConnectPannel" @mouseleave="closeConnectPannel"> </i>
-      </li>
-      <li class="shadow" @click="readHelp">
-        <i class="iconfont icon-md-help-circle"></i>
-      </li>
     </ul>
-    <section v-if="showConnect" class="connect-panel animate__animated animate__fadeIn">
-      <img :src="appConfig.site.serviceQRCodeUrl" />
-    </section>
   </div>
 </template>
 <script>
@@ -62,15 +50,7 @@ export default {
       Bus.pubEv(HOME_SCROLL_TO, 0, true);
     },
 
-   
 
-    showConnectPannel() {
-      this.showConnect = true;
-    },
-
-    closeConnectPannel() {
-      this.showConnect = false;
-    },
   },
 };
 </script>
@@ -97,28 +77,4 @@ export default {
       }
     }
   }
-  .connect-panel {
-    position: absolute;
-    left: -110px;
-    bottom: 18px;
-    width: 120px;
-    height: 120px;
-    padding: 4px;
-    background-color: var(--gray-400);
-    box-sizing: border-box;
-    &::after {
-      content: '';
-      position: absolute;
-      top: calc(50% - 8px);
-      right: -16px;
-      border-width: 8px;
-      border-color: transparent;
-      border-left-color: var(--gray-400);
-      border-style: solid;
-    }
-    img {
-      width: 100%;
-    }
-  }
-}
 </style>
